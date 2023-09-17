2fc3b94e1fd1d9a2146e30a026d815aa9fc3a11c
Lyn_Kamonchanok <kamonchanok_phu@turecorp.co.th>
2023-09-18 00:48:21 +0700

    [🎉] Create popup util

---
+++ src/utils/popup.js
@@ -0,0 +1,22 @@
+export const hed = {
+  warning: "Warning",
+  delete: "Are you sure",
+  logout: "Logout",
+  loginFail: "Login Failed",
+};
+
+export const msg = {
+  txtDelete: "You want to delete",
+  txtSectionTimeOut: "Section Timeout, Please login again.",
+  txtLoginFail: "Your username or password is incorrect, Please try again.",
+  txtFail: "Fail, Please try again.",
+};
+
+export const btn = {
+  cancel: "Cancel",
+  delete: "Delete",
+  update: "Update",
+  create: "Create",
+  logout: "Logout",
+  retry: "Try Again",
+};
